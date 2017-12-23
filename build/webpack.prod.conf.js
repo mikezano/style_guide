811c81536969d88cf7c74690a95bf1578c5a6cd9
mikezano <mikezano@gmail.com>
2017-12-23 13:30:14 -0700

    make sure publish goes to correct location

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -23,7 +23,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     path: config.build.assetsRoot,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
-    publicPath: 'https://mikezano.github.io/vue_experiments/'
+    publicPath: 'https://mikezano.github.io/style_guide/'
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
