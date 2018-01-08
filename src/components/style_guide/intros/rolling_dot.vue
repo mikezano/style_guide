f07712e969c87c7504ff596ab196bb439382facc
Michael A Manzano <michael.a.manzano@intel.com>
2018-01-08 10:10:19 -0700

    rolling dot mixin

--- src/components/style_guide/intros/rolling_dot.vue
+++ src/components/style_guide/intros/rolling_dot.vue
@@ -20,122 +20,130 @@
 
 <style lang="scss" scoped>
 
-.intro{
-	display:flex;
-	align-items:flex-start;
-	font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
-	font-size: 2em;
-	position:relative;
+@mixin intro(
+	$scale: 1, 
+	$dot_color: orange, 
+	$ads_color: hsla(0, 0%, 27%, 1), 
+	$presents_color: hsla(0, 0%, 50%, 1)){
 
-	&__travel-dot{
-		width:10px;
-		height: 10px;
-		border-radius: 5px;
-		background-color: orange;
-		position:absolute;
-		margin-top: 1em;
-		animation: move-dot 1.5s ease-in-out forwards;	width: 10px;
-	}
-	&__words{
+	$travel_distance: 235px;
+
+	.intro{
 		display:flex;
 		align-items:flex-start;
-		width:250px;
-	}
-	&__a, &__d, &__amp, &__s {
-		font-weight: bold;
-		opacity: 0;
-		text-shadow: 4px 8px lightgray;
-		color: #444;
-		animation: slide-down .2s ease-in-out forwards;
-	}
-	&__a	{animation-delay: .1s;}
-	&__d	{animation-delay: .2s;}
-	&__amp	{animation-delay: .3s;}
-	&__s	{animation-delay: .4s;}
+		font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+		font-size: 2em * $scale;
+		position:relative;
 
-	&__presents {
-		opacity: 0;
-		animation: fade-in-presents 1s ease-in-out .5s forwards;
-		margin: 0 10px;
-		color: gray;
+
+		&__travel-dot, &__dot1, &__dot2, &__dot3 {
+			width:10px * $scale;
+			height: 10px * $scale;
+			border-radius: 5px * $scale;
+		}
+		&__travel-dot{
+			background-color: $dot_color;
+			position:absolute;
+			margin-top: 1em;
+			animation: move-dot 1.5s ease-in-out forwards;
+		}
+		&__words{
+			display:flex;
+			align-items:flex-start;
+			width:$travel_distance * $scale;
+		}
+		&__a, &__d, &__amp, &__s {
+			font-weight: bold;
+			opacity: 0;
+			text-shadow: (2px * $scale) (4px * $scale) hsla(0, 0%, 83%, .7);
+			color: $ads_color;
+			animation: slide-down .2s ease-in-out forwards;
+		}
+		&__a	{animation-delay: .1s;}
+		&__d	{animation-delay: .2s;}
+		&__amp	{animation-delay: .3s;}
+		&__s	{animation-delay: .4s;}
+
+		&__presents {
+			opacity: 0;
+			animation: fade-in-presents 1s ease-in-out .5s forwards;
+			margin: 0 10px;
+			color: $presents_color;
+		}
+
+		&__dots{
+			display: flex;
+			justify-content:space-between;
+			width:40px * $scale;
+		}
+
+		&__dot1, &__dot2, &__dot3 {
+			background-color: $dot_color;
+			margin-top: .8em;
+			opacity: 0;
+			animation: jump 1s ease-in-out infinite;
+		}
+		
+		&__dot1 {animation-delay: 1.44s;}
+		&__dot2 {animation-delay: 1.54s;}
+		&__dot3 {animation-delay: 1.64s;}
 	}
 
-	&__dots{
-		display: flex;
-		justify-content:space-between;
-		width:40px;
+	@keyframes slide-down {
+		0% {
+			opacity: 0;
+			transform: translateY(-20px);
+		}
+		100% {
+			opacity: 1;
+			transform: translateY(0);
+		}
 	}
 
-	&__dot1, &__dot2, &__dot3 {
-		width: 10px;
-		height: 10px;
-		border-radius: 5px;
-		background-color: orange;
-		margin-top: .8em;
-		opacity: 0;
-		animation: jump 1s ease-in-out infinite;
+	@keyframes jump {
+		0% {
+			transform: translateY(0);
+			opacity: 1;
+		}
+		50% {
+			transform: translateY(-10px);
+			opacity: 1;
+		}
+		100% {
+			transform: translateY(0);
+			opacity: 1;
+		}
 	}
-	
-	&__dot1 {animation-delay: 1.44s;}
-	&__dot2 {animation-delay: 1.54s;}
-	&__dot3 {animation-delay: 1.64s;}
+
+	@keyframes fade-in-presents {
+		0% {
+			opacity: 0;
+			transform: translateY(-10px);
+		}
+		100% {
+			opacity: 1;
+			transform: translateY(0);
+		}
+	}
+
+	@keyframes move-dot {
+		0% {transform: translateX(0);}
+		80% {
+			transform: translateX($travel_distance * $scale);
+			margin-top:1em;
+		}
+		95% {
+			transform: translateX($travel_distance * $scale);
+			margin-top: .8em;
+			opacity: 1;
+		}
+		100% {
+			transform: translateX($travel_distance * $scale);
+			margin-top: .8em;
+			opacity: 0;
+		}
+	}//intro end
 }
 
-@keyframes slide-down {
-	0% {
-		opacity: 0;
-		transform: translateY(-20px);
-	}
-	50% {
-		font-size: 32px;
-	}
-	100% {
-		opacity: 1;
-		transform: translateY(0);
-	}
-}
-
-@keyframes jump {
-	0% {
-		transform: translateY(0);
-		opacity: 1;
-	}
-	50% {
-		transform: translateY(-10px);
-		opacity: 1;
-	}
-	100% {
-		transform: translateY(0);
-		opacity: 1;
-	}
-}
-
-@keyframes fade-in-presents {
-	0% {
-		opacity: 0;
-		transform: translateY(-10px);
-	}
-	100% {
-		opacity: 1;
-		transform: translateY(0);
-	}
-}
-
-@keyframes move-dot {
-	0% {transform: translateX(0);}
-	80% {
-		transform: translateX(250px);
-		margin-top:1em;
-	}
-	95% {
-		transform: translateX(250px);
-		margin-top: .8em;
-		opacity: 1;
-	}
-	100% {
-		transform: translateX(250px);
-		margin-top: .8em;
-		opacity: 0;
-	}
-}
+@include intro();
 </style>
