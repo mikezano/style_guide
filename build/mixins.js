279a9a53814b89f2576df0f5acddcd8ca48bd7e0
Michael A Manzano <michael.a.manzano@intel.com>
2018-01-10 17:16:17 -0700

    writing out to file with node

--- build/mixins.js
+++ build/mixins.js
@@ -8,9 +8,65 @@
 // //console.log(components_source)
 // const files = require.context(`../components/style_guide/`, true, /\.vue$/);
 // console.log(files);
-require('require-context/register')
-function importAll (r) {
-    r.keys().forEach(r);
-  }
+// require('require-context/register')
+// function importAll (r) {
+//     r.keys().forEach(r);
+//   }
    
-importAll(require.context('../../src/', true, /\.js$/));
+// importAll(require.context('../../src/', true, /\.js$/));
+const path = './src/components/style_guide/';
+const fs = require('fs');
+const mixin_regex = new RegExp("@mixin[\\s\\S]*end");
+
+// fs.readdir(path, (err, files) => {
+//   files.forEach(file => {
+
+//   //   fs.readFile('./src/App.vue', 'utf8', function(err, contents) {
+//   //     console.log(contents);
+//   // });
+
+//    // console.log(file);
+//   });
+// })
+
+var walkSync = function(dir, filelist) {
+  var fs = fs || require('fs'),
+      files = fs.readdirSync(dir);
+  filelist = filelist || [];
+  files.forEach(function(file) {
+    if (fs.statSync(dir + file).isDirectory()) {
+      filelist = walkSync(dir + file + '/', filelist);
+    }
+    else {
+      filelist.push({file: file, path: dir + file});
+    }
+  });
+  return filelist;
+};
+
+let mixins_result = "";
+let files = walkSync(path);
+files.forEach(function(file){
+  console.log(file);
+  // fs.readFile(file.path, 'utf8', function(err, contents) {
+  //   console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+  //   //console.log(contents);
+  //   let result = contents.match(mixin_regex);
+  //   mixins_result += result;
+  //   console.log(mixins_result);
+  // });
+  var contents = fs.readFileSync(file.path, 'utf8');
+  let result = contents.match(mixin_regex);
+  mixins_result += result;
+});
+//console.log("mixins:");
+console.log(mixins_result);
+
+
+fs.writeFile("./src/package/test.scss", mixins_result, function(err) {
+  if(err) {
+      return console.log(err);
+  }
+
+  console.log("The file was saved!");
+}); 
