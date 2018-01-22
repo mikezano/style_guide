0d48487b002a57aa2b03d5d674c85891e9558e56
Michael A Manzano <michael.a.manzano@intel.com>
2018-01-22 11:34:27 -0700

    clearing slate, deep selectors

--- package/README.md
+++ package/README.md
@@ -10,3 +10,6 @@ Components are sorted into their appropriate folders with pug files in each
 
 This file will need to be included at a root level and can then be refrenece with
 
+### Deep selectors
+
+If you are using child components and want them to be styled with what is indicated in the parent read about 'deep selectors' (here)[https://vue-loader.vuejs.org/en/features/scoped-css.html]
--- src/indexes/test.vue
+++ src/indexes/test.vue
@@ -1,67 +1,13 @@
 <template lang="pug">
-	.container
-		button(@click="add") Add
-		button(@click="remove") Remove
-		button(@click="shuffle") Shuffle
-		transition-group(name="list" tag="p")
-			span.list-item(v-for="item in items" v-bind:key="item")
-				| {{item}}
+
 </template>
 
 <script>
-
 export default {
-	name: 'test',
-	data () {
-		return{
-			items:[1,2,3,4,5,6,7,8,9],
-			nextNum:10
-		}
-	},
-	methods:{
-		randomIndex(){
-			return Math.floor(Math.random() * this.items.length)
-		},
-		add(){
-			this.items.splice(this.randomIndex(), 0, this.nextNum++)
-		},
-		remove(){
-			this.items.splice(this.randomIndex(), 1)
-		},
-		shuffle() {
-			this.items = this._.shuffle(this.items)
-		}
-	}
+	name: 'test'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-@import '../sass/colors.scss';
-
-.container{
-	display:flex;
-	align-items:center;
-	justify-content:center;
-}
-
-.list-item {
-	transition: all 1s;
-  display: inline-block;
-  margin-right: 10px;
-}
-.list-enter-active, .list-leave-active {
-  transition: all 10s;
-}
-.list-leave-active{
-	position:absolute;
-}
-.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
-  opacity: 0;
-  transform: translateY(30px);
-}
-.list-move{
-	transition: transform 1s;
-}
+<style lang="scss">
 
 </style>
