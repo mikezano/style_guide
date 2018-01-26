137101470c26eeb958da0ff190052dd9e9c15c95
Michael A Manzano <michael.a.manzano@intel.com>
2018-01-26 15:21:21 -0700

    remove package folder

--- package/buttons/zoom.vue
+++
@@ -1,5 +0,0 @@
-<template lang="pug">
-	div
-		button.small-zoom.zoom-btn Small Zoom
-		button.big-zoom.zoom-btn Big Zoom
-</template>
--- package/grid_layouts/right_column.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-	.container
-		.grid-container
-			.cell.a A
-			.cell.b B
-			.cell.c C
-</template>
