bb79ddb578c94b63a0e1f649b288cd51da3fd575
Michael A Manzano <michael.a.manzano@intel.com>
2018-02-02 16:38:55 -0700

    a flip button

---
+++ src/components/style_guide/buttons/flip.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+	button.flip-btn Watch this!
+</template>
+
+
+<script>
+	export default{
+		name: 'flip'
+	}
+</script>
+
+<style lang="scss" scoped>
+
+@mixin flip-btn(){
+	$selector: flip-btn;
+	&.#{$selector}{
+		$shadow-color: black;
+		box-shadow:2px 2px 4px $shadow-color;
+		transition: all 1s ease-in-out;
+		color:blue;
+		&:hover{
+			transform: rotateX(360deg);
+			box-shadow:4px 4px 8px lighten($shadow-color, 20%);
+		}
+	}
+}//zoom-btn end
+@include flip-btn();
+
+</style>
